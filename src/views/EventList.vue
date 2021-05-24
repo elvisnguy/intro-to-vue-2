c4aa855f4df9799e64c53f4409d1d6185d11cffa
Thanh Nguy <vanthanhnguy2907@gmail.com>
2021-05-24 17:53:50 +0700

    Real-world-vue2, lesson 8 -Slot.

---
+++ src/views/EventList.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h1>Events Listening</h1>
+        <EventCard/>
+        <BaseIcon/> 
+
+    </div>
+</template>
+
+<script>
+import EventCard from '@/components/EventCard.vue';
+export default {
+    components:{
+        EventCard 
+    }
+}
+</script>
